f3fd6c12f85026dcaf48457767756df1ada578f7
Thomas Bennett <trb0023@uah.edu>
2018-06-11 16:51:17 -0500

    Finally fixed differ

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from datetime import datetime
 import requests
 from difflib import *
+import html
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,10 +11,8 @@ def homepage():
 
 	r = requests.get('http://www.cansatcompetition.com/mission.html')
 
-	reference = """
-<!DOCTYPE HTML>
+	reference = """<!DOCTYPE HTML>
 <html>
-
 <head>
   <title>CanSat Competition</title>
   <meta name="description" content="Cansat Competition" />
@@ -23,29 +22,28 @@ def homepage():
   <!-- modernizr enables HTML5 elements and feature detects -->
   <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
 </head>
-
 <body>
   <div id="main">
     <header>
       <div id="logo">
-	<div id="logo_text">
-	  <!-- class="logo_colour", allows you to change the colour of the text -->
-	  <h1><a href="index.html">CanSat Competition</a></h1>
-	  <h2>Mission</h2>
-	</div>
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html">CanSat Competition</a></h1>
+          <h2>Mission</h2>
+        </div>
       </div>
       <nav>
-	<div id="menu_container">
-	  <ul class="sf-menu" id="nav">
-	    <li><a href="index.html">Home</a></li>
-	    <li><a href="mission.html">Mission</a></li>
-	    <li><a href="teams.html">Teams</a></li>
+        <div id="menu_container">
+          <ul class="sf-menu" id="nav">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="mission.html">Mission</a></li>
+            <li><a href="teams.html">Teams</a></li>
 	    <li><a href="winners.html">2017 Winners</a></li>
 	    <li><a href="photos.html">Photos</a></li>
 			<li><a href="documents.html">Documents</a></li>
-	     <li><a href="mailto:cansatcompetition@gmail.com?subject=CanSat Competition">Contact Us</a></li>
-	  </ul>
-	</div>
+             <li><a href="mailto:cansatcompetition@gmail.com?subject=CanSat Competition">Contact Us</a></li>
+          </ul>
+        </div>
       </nav>
     </header>
     <div id="site_content">
@@ -61,18 +59,16 @@ def homepage():
 			<h2><a href="docs/Cansat_environmental_tests_r1.3.pdf">Environmental Test Guide</a></h2>
 			<h2><a href="docs/mission_operations_manual_2016.doc">Mission Operations Manual</a></h2>
 			<h2><a href="docs/pfr_outline_2018.pdf">Post Flight Review Outline</a></h2>
-	</div>
+        </div>
       </div>
       <div class="content">
-	<h1>Introduction</h1>
+        <h1>Introduction</h1>
 <p>The 2018 mission simulates a space probe (CanSat) entering a planetary atmosphere. The probe shall carry a single large hen's egg. The egg must survive all portions of flight. The operation sequence shall be:
 <p>1. The probe is launched to an altitude of 670 meters to 725 meters and then deployed from the rocket. Orientation of deployment is not controlled and is most definitely violent.
 <p>2. Once deployed, the probe shall open an aero-braking heat shield. The descent rate shall be kept at 10 to 30 meters/sec. The aero-braking probe must maintain a stable orientation with the heat shield facing the direction of descent during descent. Tumbling is not allowed. Active control surfaces or other non pyrotechnic mechanisms can be used to maintain orientation.
 <p>3. At an altitude of 300 meters, the probe shall release the aero-braking heat shield and simultaneously deploy a parachute to reduce the descent rate of 5 meters/sec.
 <p>4. The probe shall land leaving the egg intact.
 <p>The probe shall include sensors for tracking altitude using air pressure, external temperature, battery voltage and GPS position. A compartment shall be included to hold a large hen's egg. The egg will simulate a delicate instrument.
-
-
       </div>
     </div>
     <div id="scroll">
@@ -96,14 +92,14 @@ def homepage():
   </script>
 </body>
 </html>
-""".strip("	 ").splitlines(1)
+""".splitlines(1)
 	print(r.text)
 	print(reference)
 
-	sitetext = r.text.strip("	 ").splitlines(1)
+	sitetext = r.text.splitlines(1)
 
-	differ = HtmlDiff(tabsize=8, wrapcolumn=80)
-	html = differ.make_file(sitetext, reference, context=False )
+	differ = HtmlDiff(tabsize=8, wrapcolumn=0)
+	htmltable = differ.make_file(reference, sitetext, context=False )
 
 	return """
 	<h1>Hello heroku</h1>
@@ -111,7 +107,7 @@ def homepage():
 
 	<img src="http://loremflickr.com/600/400">
 
-	""".format(time=the_time)+html
+	""".format(time=the_time)+htmltable
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
