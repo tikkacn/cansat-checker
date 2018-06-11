bc445e74632b2fe9012f10e9d33603fea8776e70
Thomas Bennett <trb0023@uah.edu>
2018-06-11 14:34:18 -0500

    Removed stuff that was breaking things

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 import requests
-from urllib3 import Request, urlopen
 
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-
-
-
-#request = Request('https://realemail.expeditedaddons.com/?api_key=' + os.environ['REALEMAIL_API_KEY'] + '&email=email%40example.org&fix_typos=false')
-
-#response_body = urlopen(request).read()
-#print response_body
-
 
 
 
