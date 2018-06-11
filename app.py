051f1feb2fde467d0f8275d694d8a91459ff4da5
Thomas Bennett <trb0023@uah.edu>
2018-06-11 15:40:25 -0500

    changing

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ app = Flask(__name__)
 def homepage():
 	the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-	return """
-	<h1>Hello heroku</h1>
-	<p>It is currently {time}.</p>
-
-	<img src="http://loremflickr.com/600/400">
-
-	""".format(time=the_time)
-
-if __name__ == '__main__':
 	r = requests.get('http://www.cansatcompetition.com/mission.html')
 
 	reference = """
@@ -105,10 +96,19 @@ if __name__ == '__main__':
 </body>
 </html>
 """
-	
 	print(r.text)
 	print(reference)
 	print(r.text == reference)
+
+	return """
+	<h1>Hello heroku</h1>
+	<p>It is currently {time}.</p>
+
+	<img src="http://loremflickr.com/600/400">
+
+	""".format(time=the_time)
+
+if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
 
 
