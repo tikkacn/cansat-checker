5a9c2084101eeb3f4b49883d630d4ddea03fa3ea
Thomas Bennett <trb0023@uah.edu>
2018-06-11 14:45:34 -0500

    Swap to flask

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+from flask import Flask
+from datetime import datetime
 import requests
+app = Flask(__name__)
 
-from django.core.wsgi import get_wsgi_application
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-application = get_wsgi_application()
+    r = requests.get('http://www.cansatcompetition.com/mission.html')
+    print(r.text)
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+
+    """.format(time=the_time)+r.text
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
 
 
 
-r = requests.get('http://www.cansatcompetition.com/mission.html')
-print(r.text)
+
