fb8c9792efec402fce7ca17f6b3712a167c60d37
Thomas Bennett <trb0023@uah.edu>
2018-06-11 17:46:41 -0500

    change order

--- sitecheck.py
+++ sitecheck.py
@@ -2,6 +2,16 @@ import requests
 import html
 import smtplib
 
+def sendmail():
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login("cansatstatuschecker@gmail.com", "cansat1234")
+ 
+	msg = "(Just a drill) CanSat has been Updated!"
+	for address in maillist:
+		server.sendmail("cansatstatuschecker@gmail.com", address, msg)
+	server.quit()
+
 maillist = ['trbinsc@gmail.com','jrd0023@uah.edu']
 
 r = requests.get('http://www.cansatcompetition.com/mission.html')
@@ -14,15 +24,7 @@ else:
 
 sendmail()
 
-def sendmail():
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login("cansatstatuschecker@gmail.com", "cansat1234")
- 
-	msg = "(Just a drill) CanSat has been Updated!"
-	for address in maillist:
-		server.sendmail("cansatstatuschecker@gmail.com", address, msg)
-	server.quit()
+
 
 
 
