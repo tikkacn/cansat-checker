3fb22f3d71b21d2b7b4050359a47cf10889910b2
Thomas Bennett <trb0023@uah.edu>
2018-06-12 14:47:22 -0500

    Adding another person

--- sitecheck.py
+++ sitecheck.py
@@ -11,7 +11,7 @@ from email.mime.multipart import MIMEMultipart
 import random
 import datetime
 
-maillist = ['trb0023@uah.edu','jrd0027@uah.edu','jh0115@uah.edu','cac0049@uah.edu','jrh0056@uah.edu','dc0069@uah.edu','anj0023@uah.edu','mab0086@uah.edu','mwb0015@uah.edu','nmg0006@uah.edu','cd0040@uah.edu']
+maillist = ['trb0023@uah.edu','jrd0027@uah.edu','jh0115@uah.edu','cac0049@uah.edu','jrh0056@uah.edu','dc0069@uah.edu','anj0023@uah.edu','mab0086@uah.edu','mwb0015@uah.edu','nmg0006@uah.edu','cd0040@uah.edu','ajb0042@uah.edu']
 
 ORG_EMAIL   = "@gmail.com"
 FROM_EMAIL  = "cansatstatuschecker" + ORG_EMAIL
